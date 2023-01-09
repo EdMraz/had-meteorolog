50bafdf660fd2c79e30237cfcdc12b706d376016
EdMraz <eduard.mraz05@gmail.com>
2023-01-09 23:57:47 +0100

    initial commit

--- meteorolog.py
+++ meteorolog.py
@@ -29,5 +29,14 @@ for riadok in riadky:
     x = zoz_riadok[-2]
     c.append(float(x))
     counter+=1
-
 print("Priemerná teplota:", round((sum(c)/counter),2))
+
+max_temp = b[0]
+nazov_stanice = ""
+for riadok in riadky:
+    zoz_riadok = riadok.split()
+    temp = zoz_riadok[-2]
+    if float(max_temp) < float(temp):
+        max_temp = temp
+        nazov_stanice = zoz_riadok[-5]
+print("Nazov stanice s najvyssou teplotou:", nazov_stanice)
